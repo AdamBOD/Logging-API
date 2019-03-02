d4dea5b4eca681c270c5fd3862216a33884c3f76
Adam BOD <adambarryodonovan@gmail.com>
2019-03-02 21:35:47 +0000

    Added CORS to API

--- lib/app.ts
+++ lib/app.ts
@@ -1,6 +1,7 @@
 import * as express from "express";
 import * as bodyParser from "body-parser";
 import * as mongoose from 'mongoose';
+import * as cors from 'cors';
 import { MongoClient } from 'mongodb';
 
 import {Routes} from './routes/routes'
@@ -14,6 +15,7 @@ class App {
 
     constructor() {
         this.app = express();
+        this.app.use (cors());
         this.config();
         this.routePrv.routes(this.app); 
         this.mongoSetup(); 
